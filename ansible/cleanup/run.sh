729f61177008928c3c86d173d99ea05547b519dc
Reza Rahim <reza.rahim@gmail.com>
2025-04-27 17:15:20 +0000

    cleanup

--- ansible/cleanup/run.sh
+++ ansible/cleanup/run.sh
@@ -1,2 +1,4 @@
 ansible-playbook -i  ../inventory.ini zookeeper.yaml 
 #ansible-playbook -i  ../inventory.ini minio.yaml 
+ansible-playbook -i  ../inventory.ini hadoop.yaml 
+ansible-playbook -i  ../inventory.ini spark.yaml 
