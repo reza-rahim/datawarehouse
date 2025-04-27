1ffdb5b7f2bfc0bda201d5856f8ccf7afc8871ac
Reza Rahim <reza.rahim@gmail.com>
2025-04-27 21:20:25 +0000

    cert

--- ansible/cert/README.md
+++ ansible/cert/README.md
@@ -2,11 +2,18 @@
 pki/
 ├── ca/
 │   ├── rootCA.key
-│   └── rootCA.pem
+│   └── rootCA.crt
 ├── server/
 │   ├── wildcard.key
 │   ├── wildcard.csr
 │   ├── wildcard.crt
 │   └── server_cert_ext.cnf
+
+```
+
+```
+cp pki/server/server.crt ../roles/cert/templates/server.crt
+cp pki/server/server.key ../roles/cert/templates/server.key
+cp pki/ca/rootCA.crt  /usr/local/share/ca-certificates/rootCA.crt
 
 ```
--- ansible/cert/ssl.sh
+++ ansible/cert/ssl.sh
@@ -23,7 +23,7 @@ echo "Creating Root CA..."
 openssl genrsa -out ca/rootCA.key 4096
 
 # === Root CA Certificate ===
-openssl req -x509 -new -nodes -key ca/rootCA.key -sha256 -days $DAYS_VALID_CA -out ca/rootCA.pem \
+openssl req -x509 -new -nodes -key ca/rootCA.key -sha256 -days $DAYS_VALID_CA -out ca/rootCA.crt \
 -subj "/C=$COUNTRY/ST=$STATE/L=$CITY/O=$ORG/OU=$OU/CN=$ROOT_CA_CN"
 
 echo "Root CA created."
@@ -54,14 +54,14 @@ EOF
 echo "Signing server server certificate with Root CA..."
 
 # === Sign the Server CSR with Root CA ===
-openssl x509 -req -in server/server.csr -CA ca/rootCA.pem -CAkey ca/rootCA.key -CAcreateserial \
+openssl x509 -req -in server/server.csr -CA ca/rootCA.crt -CAkey ca/rootCA.key -CAcreateserial \
 -out server/server.crt -days $DAYS_VALID_SERVER -sha256 -extfile server/server_cert_ext.cnf
 
 echo "Wildcard certificate signed."
 
 # === Done! Show locations ===
 echo "=== Certificates Created Successfully ==="
-echo "Root CA Certificate: $(pwd)/ca/rootCA.pem"
+echo "Root CA Certificate: $(pwd)/ca/rootCA.crt"
 echo "Root CA Private Key: $(pwd)/ca/rootCA.key"
 echo "Wildcard Server Private Key:  $(pwd)/server/server.key"
 echo "Wildcard Server Certificate:  $(pwd)/server/server.crt"
