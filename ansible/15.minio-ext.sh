53e38c28a0ea3f7cfb05e468cea1d5ff3fe28697
Reza Rahim <reza.rahim@gmail.com>
2025-06-01 16:43:04 +0000

    bucket

--- ansible/15.minio-ext.sh
+++ ansible/15.minio-ext.sh
@@ -2,3 +2,7 @@ source 00.vip_fqdn.sh
 ansible-playbook -i inventory.ini \
 -e fqdn=$FQDN \
 minio-ext.yaml;	
+
+eval "$(/etc/secret/decrypt_secret_eval.sh)"
+mc alias set s3 https://minio.$FQDN:9000 "$MINIO_ROOT_USER" "$MINIO_ROOT_PASSWORD"
+mc ls s3
